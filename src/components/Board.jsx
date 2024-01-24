c615f70704372458f46a7fb53a8f2956a5c5f25a
Fatih Kabay <mfthkabay@gmail.com>
2024-01-24 22:45:30 +0300

    board and slot component

---
+++ src/components/Board.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+import Slot from "./Slot";
+
+function Board() {
+  return (
+    <>
+      <div id="board"></div>
+    </>
+  );
+}
+
+export default Board;
---
+++ src/components/Slot.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Slot() {
+  return <div className="slot"s></div>;
+}
+
+export default Slot;
