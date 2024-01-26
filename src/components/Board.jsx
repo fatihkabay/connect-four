83a5f3eca927f83af39d423a6f54619ddc7ca456
Fatih Kabay <mfthkabay@gmail.com>
2024-01-26 19:05:21 +0300

    board click

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Slot from "./Slot";
+import "../App.css";
 
 function Board() {
   const [board, setBoard] = useState([
@@ -14,10 +15,21 @@ function Board() {
   const [oppPlayer, setOppPlayer] = useState("O");
   const [gameOver, setGameOver] = useState(false);
 
+  const handleClick = (e) => {
+    const column = e.target.getAttribute("x");
+    const row = board.findIndex((rowArr, index) => {
+      return rowArr[column] !== "" || index === board.length - 1;
+    });
+  };
+
+  if (row !== board.length - 1) row -= 1;
+  if (board[row][column] !== "") row -= 1;
+
   return (
     <>
-      <h2></h2>
-      <div id="board">
+      {gameOver && <h1>{oppPlayer === "X" ? "Red" : "Black"} Wins</h1>}
+      <h2 id="playerDisplay">{currPlayer === "X" ? "Red" : "Black"} Move</h2>
+      <div id="board" onClick={gameOver ? null : handleClick}>
         {board.map((row, i) => {
           return row.map((ch, j) => {
             return <Slot ch={ch} y={i} x={j} />;
