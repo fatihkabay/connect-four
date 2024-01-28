22d31ec3983bd70e52d4b273dc855a62ca382675
Fatih Kabay <mfthkabay@gmail.com>
2024-01-28 15:48:10 +0300

    win variations

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -15,15 +15,111 @@ function Board() {
   const [oppPlayer, setOppPlayer] = useState("O");
   const [gameOver, setGameOver] = useState(false);
 
+  //win variations
+  const checkWin = (row, column, ch) => {
+    //row increases, column stays the same
+    try {
+      if (board[row + 1][column] === ch) {
+        if (board[row + 2][column] === ch) {
+          if (board[row + 3][column] === ch) {
+            return true;
+          }
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+    //row increases, column increases
+    try {
+      if (board[row + 1][column + 1] === ch) {
+        if (board[row + 2][column + 2] === ch) {
+          if (board[row + 3][column + 3] === ch) {
+            return true;
+          }
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+    //row increases, column decreases
+    try {
+      if (board[row + 1][column - 1] === ch) {
+        if (board[row + 2][column - 2] === ch) {
+          if (board[row + 3][column - 3] === ch) {
+            return true;
+          }
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+    //row stays at same, column increases
+    try {
+      if (board[row][column + 1] === ch) {
+        if (board[row][column + 2] === ch) {
+          if (board[row][column + 3] === ch) {
+            return true;
+          }
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+    //row stays at same, column decreases
+    try {
+      if (board[row][column - 1] === ch) {
+        if (board[row][column - 2] === ch) {
+          if (board[row][column - 3] === ch) {
+            return true;
+          }
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+    //row decreases, column decreases
+    try {
+      if (board[row - 1][column - 1] === ch) {
+        if (board[row - 2][column - 2] === ch) {
+          if (board[row - 3][column - 3] === ch) {
+            return true;
+          }
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+    //row decreases, column increases
+    try {
+      if (board[row - 1][column + 1] === ch) {
+        if (board[row - 2][column + 2] === ch) {
+          if (board[row - 3][column + 3] === ch) {
+            return true;
+          }
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  const updateBoard = (row, column, ch) => {
+    setBoard((prev) => {
+      const boardCopy = [...prev];
+      boardCopy[row][column] = ch;
+      return boardCopy;
+    });
+    return checkWin(row, column, ch);
+  };
+
   const handleClick = (e) => {
     const column = e.target.getAttribute("x");
     const row = board.findIndex((rowArr, index) => {
       return rowArr[column] !== "" || index === board.length - 1;
     });
+    if (row !== board.length - 1) row -= 1;
+    if (board[row][column] !== "") row -= 1;
   };
-
-  if (row !== board.length - 1) row -= 1;
-  if (board[row][column] !== "") row -= 1;
 
   return (
     <>
